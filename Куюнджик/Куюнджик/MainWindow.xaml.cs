fbf0f4f6643161d7c9e18d4e1be36a48d718d78c
Maksim <93037214+LetY0uDown@users.noreply.github.com>
2022-01-11 11:24:15 +1000

    Add files via upload

---
+++ Куюнджик/Куюнджик/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using StringMath;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Куюнджик
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += $"{(sender as Button).Tag}";
+        }
+
+        private void bDEL_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+            textResult.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Calculator calculator = new Calculator();
+            try
+            {
+                double result = calculator.Evaluate(textBox.Text);
+                textResult.Text = "= " + result.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Вы ввели фигню");
+            }
+        }
+    }
+}
